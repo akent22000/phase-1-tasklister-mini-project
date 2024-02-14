53f4801a468ecac883be00b487a4b54cf3ef324a
amandalocher <akent22000@gmail.com>
2024-02-14 14:15:55 -0500

    complteted lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//clicking of the submit button. To do this, you'll need to listen for a submit event on the <form> element. In order to prevent the default behavior of the submit event, when our event listener "sees" the event, it needs to invoke the preventDefault() method on it.
+
+document.addEventListener("DOMContentLoaded", myFunction1);
+
+function myFunction1() {
+  document.addEventListener("click", myFunction2);
+
+  function myFunction2() {
+    document.getElementById('create-task-form').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+
+      document.addEventListener("click", function(event) {
+        // console.log("YOU CLICKED IT");
+        event.preventDefault();
+      });
+
+
+  })
+  
+}
+}
+
+
